be46c52bf64bbaffee8e9e75e728004baf17835e
cacara82 <cacara890@gmail.com>
2025-10-08 12:18:51 +0200

    afegits estils per la taula, logo, comentat millor i avís per pantalles petites

--- src/pages/MappingDashboard.tsx
+++ src/pages/MappingDashboard.tsx
@@ -5,8 +5,12 @@ import { MappingTable } from '../utils.tsx'
 export default function MappingDashboard() {
     return (
         <div className="flex flex-col items-center columns-1">
-            <h1 className="text-4xl text-center m-10 md:text-5xl">Mapping Dashboard</h1>
-            <div className="m-5">
+            <div className='md:flex md:items-center md:gap-10'>
+                <img src="logo.png" alt="Logo" className='w-120 h-50' />
+                <h1 className="text-4xl text-center md:mt-10 mb-5 md:text-5xl">Database Anonymisation · Project Dashboard</h1>
+            </div>
+            
+            <div className="mb-5">
                 <p className="text-center"><strong>Som a dia: </strong><Clock /></p>
             </div>
             <div className='m-5'>
--- src/utils.tsx
+++ src/utils.tsx
@@ -2,46 +2,69 @@ import { useEffect, useState } from "react";
 import Papa from "papaparse";
 import csvFilename from "/src/assets/data.csv?raw";
 
-// Attributes
+// Variables generals
 var now = new Date();
-//var csvFilename = "src/assets/data.csv";
 
-// FUNCTIONS
+// FUNCIONS
+
+/**
+ * Component que utilitza variables d'estat i effect
+ * per generar un rellotge amb la data actual que 
+ * s'actualitza cada segon. Format local (dd/mm/yyyy).
+ * @returns component HTML per implementar
+ */
 export function Clock() {
+
+    // Variables
     const [hora, setHora] = useState(new Date());
-    // useEffect
+    
+    // useEffect per re-renderitzar la hora en la mateixa instància de pàgina
     useEffect(() => {
+
+        // Interval que actualitza la hora cada 1000ms (1s)
         const interval = setInterval(() => {
             setHora(new Date())
         }, 1000)
+
         return () => clearInterval(interval);
+
     }, [])
+
+    // Return the HTML component
     return (
         <div>
             <p className="text-center border-2 rounded-2xl p-4 m-2">{now.toLocaleDateString()} - {hora.toLocaleTimeString()}</p>
         </div>
     );
+
 }
 
+/**
+ * Aquesta funció genera genera una taula
+ * personalitzada del CSV de Web Dashboard 
+ * del projecte REDCap. Per cada projecte,
+ * generarà una pàgina d'aquell projecte.
+ * @returns component HTML per implementar
+ */
 export function MappingTable() {
     
-    // Attributes
-    const [csvData, setCsvData] = useState([]);
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(true);
+    // Variables
+    const [csvData, setCsvData] = useState([]); // estat per carregar les dades del CSV
+    const [error, setError] = useState(""); // estat per carregar si tenim un error amb la lectura del CSV
+    const [loading, setLoading] = useState(true); // estat per carregar si la pàgina està carregant o no
 
-    // useEffect to parse the csv file
+    // useEffect per parsejar l'arxiu CSV amb Papa Parse
     useEffect(() => {
         Papa.parse(csvFilename, {
-            download: false,
+            download: false, // no el descarreguem, el tenim importat
             header: true,
             skipEmptyLines: true,
-            complete: (results: any) => {
+            complete: (results: any) => { // un cop es completa el parse
                 console.log("CSV carregat, contenint les dades:", results.data);
                 setCsvData(results.data);
                 setLoading(false); 
             },
-            error: (err: any) => {
+            error: (err: any) => { // si hi ha algun error al procés
                 console.error("Error intern de Papa Parse: ", err);
                 setError("Error al carregar l'arxiu CSV.");
                 setLoading(false);
@@ -49,50 +72,56 @@ export function MappingTable() {
         })
     }, []);
     
-    
+    // Mentre carrega
     if (loading) {
         return (
             <p className="text-center font-bold text-blue-600">Carregant les dades...</p>
         )
     }
 
+    // Si hi ha error
     if (error) {
         return (
             <p className="text-center font-bold text-red-600">{error}</p>
         )
     }
 
+    // Ens guardem el header del csv (la row de les variables)
     var headers = Object.keys(csvData[0]);
 
-    // Return the table
+    // Retornem la taula mapejada en HTML (avís si la pantalla es petita)
     return (
-        <table className="border-2 text-center table-fixed text-sm">
-            <thead className="border-2">
-                <tr className="border-2">
-                    {headers.map((header) => (
-                        <th className="border-2">
-                            {header}
-                        </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody className="border-2">
-                {csvData.map((row, i) => (
-                    <tr key={i} className="border-2">
+        <div>
+            <p className="font-bold text-red-600 text-center m-10 p-5 border-3 rounded-lg md:hidden">Per raons de tamany, el dashboard no està disponible a pantalles petites. Prova d'utilitzar una pantalla més gran.</p>
+            <table className="hidden text-center table-fixed w-100 text-xs md:text-sm md:w-full md:block">
+                <thead className="border-2">
+                    <tr className="border-2">
                         {headers.map((header) => (
-                          <td className="border-2">
-                            {header === "main_dm_email" ? (
-                                <a href={`mailto:${row[header]}`} className="text-blue-600 hover:underline">
-                                    {row[header]}
-                                </a>
-                            ) : (
-                                row[header]
-                            )}
-                          </td>  
+                            <th className="border-2 px-4 py-2 bg-orange-500">
+                                {header}
+                            </th>
                         ))}
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody className="border-2">
+                    {csvData.map((row, i) => (
+                        <tr key={i} className="border-2 px-4 py-2">
+                            {headers.map((header) => (
+                            <td className="border-2 px-4 py-2">
+                                {header === "main_dm_email" ? (
+                                    <a href={`mailto:${row[header]}`} className="text-blue-600 hover:underline">
+                                        {row[header]}
+                                    </a>
+                                ) : (
+                                    row[header]
+                                )}
+                            </td>  
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div> 
     )
+
 }
